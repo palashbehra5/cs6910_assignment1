cbc7743d70a68827a1346a4cc9bbb93455219b40
lord of the frenzied flame <40783095+palashbehra5@users.noreply.github.com>
2023-02-27 22:37:36 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -7,8 +7,6 @@ def accuracy(y_pred,y_true):
 def confusion_matrix(y_pred,y_true):
 
     matrix = np.zeros((10,10))
-
-    for i in range(len(y_true)):
-        matrix[y_true[i]][y_pred[i]]+=1
+    for i in range(len(y_true)): matrix[y_true[i]][y_pred[i]]+=1
 
     return matrix
