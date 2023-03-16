42fd5ced0b386442a9e507de4c20e2a78dfb72fb
lord of the frenzied flame <40783095+palashbehra5@users.noreply.github.com>
2023-03-16 09:25:17 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -5,29 +5,29 @@ def parse():
   parser = argparse.ArgumentParser(description='CS6910 - Assignment 1 :')
 
   # String type arguments
-  parser.add_argument('-wp','--wandb_project', type=str, default = "myprojectname")
-  parser.add_argument('-we','--wandb_entity', type=str, default = "myname")
+  parser.add_argument('-wp','--wandb_project', type=str, default = "cs6910_assignment1")
+  parser.add_argument('-we','--wandb_entity', type=str, default = "run")
   parser.add_argument('-d','--dataset', type=str, default = "fashion_mnist")
   parser.add_argument('-l','--loss', type=str, default = "cross_entropy")
-  parser.add_argument('-o','--optimizer', type=str, default = "sgd")
+  parser.add_argument('-o','--optimizer', type=str, default = "adam")
   parser.add_argument('-w_i','--weight_init', type=str, default = "xavier")
   parser.add_argument('-a','--activation', type=str, default = "relu")
   parser.add_argument('-opt','--output', type=str, default = "softmax")
 
   # Integer type arguments
-  parser.add_argument('-e','--epochs', type=int, default = 5)
-  parser.add_argument('-nhl','--num_layers', type=int, default = 2)
+  parser.add_argument('-e','--epochs', type=int, default = 10)
+  parser.add_argument('-nhl','--num_layers', type=int, default = 4)
   parser.add_argument('-sz','--hidden_size', type=int, default = 128)
-  parser.add_argument('-b','--batch_size', type=int, default = 128)
+  parser.add_argument('-b','--batch_size', type=int, default = 16)
 
   # Float type arguments
-  parser.add_argument('-lr','--learning_rate',type=float, default = 1e-1)
-  parser.add_argument('-m','--momentum',type=float, default = 0.6)
-  parser.add_argument('-beta','--beta',type=float, default = 0.6)
-  parser.add_argument('-beta1','--beta1',type=float, default = 0.5)
-  parser.add_argument('-beta2','--beta2',type=float, default = 0.5)
+  parser.add_argument('-lr','--learning_rate',type=float, default = 1e-4)
+  parser.add_argument('-m','--momentum',type=float, default = 0.9)
+  parser.add_argument('-beta','--beta',type=float, default = 0.9)
+  parser.add_argument('-beta1','--beta1',type=float, default = 0.99)
+  parser.add_argument('-beta2','--beta2',type=float, default = 0.99)
   parser.add_argument('-eps','--epsilon',type=float, default = 0.000001)
-  parser.add_argument('-w_d','--weight_decay',type=float, default =.0)
+  parser.add_argument('-w_d','--weight_decay',type=float, default = 1e-3)
 
   args = parser.parse_args()
 
